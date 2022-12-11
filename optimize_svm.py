01691256fdc345d0bc53e46542a5c40551157e46
Māris Nartišs <maris.gis@gmail.com>
2022-12-11 20:03:15 +0200

    SVM optimization code

--- optimize_svm.py
+++ optimize_svm.py
@@ -7,16 +7,26 @@ from pymoo.core.problem import StarmapParallelization
 from pymoo.optimize import minimize
 from pymoo.algorithms.soo.nonconvex.pso import PSO
 
-from problems import SMAPProblem, listener
+from problems import SVMProblem, listener
 
 
 def get_X(pop_size, n_var, config):
+    # TODO: make universal (see SMAP get_X)
     X = np.empty((pop_size, n_var), dtype=np.float64)
-    r = (config["xu"][1] / config["xl"][1]) ** (1 / (pop_size - 1))
-    s0 = (config["xu"][0] - config["xl"][0]) / pop_size
+    s = int(math.sqrt(pop_size))
+    C = 0.1
+    Cs = []
+    Gs = []
+    for i in range(s):
+        G = 0.5
+        for j in range(s):
+            Cs.append(C)
+            Gs.append(G)
+            G = G * 4
+        C = C * 10
     for i in range(pop_size):
-        X[i, 0] = int(config["xl"][0] + i * s0)
-        X[i, 1] = int(r**i)
+        X[i, 0] = Cs[i]
+        X[i, 1] = Gs[i]
     return X
 
 
@@ -25,9 +35,9 @@ pop_size = 20
 n_threads = 5
 n_gens = 10
 n_var = 2
-_dir = "/home/marisn/Projekti/i.svm and r.smooth/"
-csv_file = _dir + "train_smap.csv"
-final_file = _dir + "final_smap"
+_dir = ""
+csv_file = _dir + "train_svm.csv"
+final_file = _dir + "final_svm"
 config = {
     "group_t": "p1",
     "subgroup_t": "RGBI",
@@ -35,10 +45,10 @@ config = {
     "subgroup_v": "RGBI",
     "training": "training",
     "signature": "p1_rgbi",
-    "output": "p1_rgbi_smap",
+    "output": "p1_rgbi_svm_c_rbf",
     "validation": "validation",
-    "xl": [2, 2],
-    "xu": [30, 4096],
+    "xl": [0.1, 0.1],
+    "xu": [10000000.0, 100000.0],
     "n_var": n_var,
 }
 
@@ -48,7 +58,7 @@ manager = Manager()
 q = manager.Queue()
 watcher = pool.apply_async(listener, (q, csv_file))
 runner = StarmapParallelization(pool.starmap)
-problem = SMAPProblem(elementwise_runner=runner, config=config, q=q)
+problem = SVMProblem(elementwise_runner=runner, config=config, q=q)
 algorithm = PSO(
     pop_size=pop_size,
     sampling=get_X(pop_size, n_var, config),
